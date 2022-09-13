ce233375c7c7ec7ad8ea5e268951ed778a887b5c
Yutong Liu <yutong13@ualberta.ca>
2022-09-12 18:08:17 -0600

    update python script to download itself

--- lab1.py
+++ lab1.py
@@ -1,3 +1,4 @@
 import requests
 # print(requests.__version__)
-print(requests.get("https://www.google.com").text)
+# print(requests.get("https://www.google.com").text)
+print(requests.get("https://raw.githubusercontent.com/Yutong13/Cmput404_lab1/main/lab1.py").text)
