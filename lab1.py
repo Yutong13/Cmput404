f991c80973150638ebe4986f7aaa9afb0816a300
Yutong Liu <yutong13@ualberta.ca>
2022-09-19 18:06:46 -0600

    Create lab2 folder

--- lab1.py
+++
@@ -1,4 +0,0 @@
-import requests
-# print(requests.__version__)
-# print(requests.get("https://www.google.com").text)
-print(requests.get("https://raw.githubusercontent.com/Yutong13/Cmput404_lab1/main/lab1.py").text)
